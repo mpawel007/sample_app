1aa60e6b6ab3bb23bbb957795265df10de221c2c
Pawel Madejski <p.madejski@medical-tribune.pl>
2013-04-15 09:58:17 +0200

    Examples from chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,12 +25,22 @@ describe "UserPages" do
     	it "should not create a user" do
   			expect { click_button submit }.not_to change( User, :count )
   		end
+
+      describe "after submission" do
+        before{ click_button submit }
+        
+        it { should have_selector('title', text: full_title('Sign up') ) }
+        it { should have_content('error') }
+      end
   	end
 
   	describe "with properly filled form" do
+
+      let(:email){ "user@example.com"}
+
   		before do
 	  		fill_in "Name", with: "Example Name"
-	  		fill_in "Email", with: "user@example.com"
+	  		fill_in "Email", with: email
 	  		fill_in "Password", with: "foobar"
 	  		fill_in "Password confirmation", with: "foobar"
 	  	end
@@ -38,6 +48,15 @@ describe "UserPages" do
 	  	it "should create user" do
   			expect { click_button submit }.to change( User, :count ).by(1)
   		end
+
+      describe "after saving the user" do 
+        before { click_button submit }
+
+        let(:user) { User.find_by_email( email ) }
+
+        it { should have_selector('title', text: full_title(user.name) ) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
   	end
   end
 
